d9993fc4fc7baa9f46cd4db6419386a73d35879a
System Administrator <root@192.168.1.7>
2022-07-13 19:53:07 +0530

    Bug Fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import SSRProvider from "react-bootstrap/SSRProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/lib/main.scss";
-import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <AnimatePresence exitBeforeEnter initial={false}>
-      <SSRProvider>
-        <Component {...pageProps} key={router.route} />
-      </SSRProvider>
-    </AnimatePresence>
+    <SSRProvider>
+      <Component {...pageProps} key={router.route} />
+    </SSRProvider>
   );
 }
 
